3d91af0a959fa20f634ab5c4f19a4d691112ba13
takashi310 <kawaset@janelia.hhmi.org>
2024-05-23 18:15:44 -0400

    validate and retry: support other compressions 5

--- src/main/java/net/preibisch/bigstitcher/spark/N5BlockValidateAndRetry.java
+++ src/main/java/net/preibisch/bigstitcher/spark/N5BlockValidateAndRetry.java
@@ -5,6 +5,7 @@ import net.imglib2.img.Img;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Intervals;
 import net.preibisch.bigstitcher.spark.util.DataTypeUtil;
 import org.janelia.saalfeldlab.n5.*;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
@@ -37,6 +38,45 @@ public class N5BlockValidateAndRetry {
 
         return true;
     }
+
+    public static boolean ValidateN5Blocks(net.imglib2.RandomAccessibleInterval source,
+                                           org.janelia.saalfeldlab.n5.N5Writer n5,
+                                           String dataset,
+                                           long[] gridOffset,
+                                           int retry) {
+        final DatasetAttributes attributes = n5.getDatasetAttributes(dataset);
+        final int n = source.numDimensions();
+        final long[] max = Intervals.maxAsLongArray(source);
+        final long[] offset = new long[n];
+        final long[] gridPosition = new long[n];
+        final int[] blockSize = attributes.getBlockSize();
+        final int[] intCroppedBlockSize = new int[n];
+        final long[] longCroppedBlockSize = new long[n];
+
+        for (int d = 0; d < n;) {
+            for (int dd = 0; dd < max.length; ++dd) {
+                longCroppedBlockSize[dd] = Math.min(blockSize[dd], max[dd] - offset[dd] + 1);
+                intCroppedBlockSize[dd] = (int)longCroppedBlockSize[dd];
+                gridPosition[dd] = offset[dd] / blockSize[dd] + gridOffset[dd];
+            }
+            System.out.println( "validating block "+ Arrays.toString(gridPosition));
+            if (!ValidateN5Block(n5, dataset, gridPosition)) {
+                System.err.println( "The n5 block "+ Arrays.toString(gridPosition) +" is corrupted. retrying... " + retry);
+                return false;
+            }
+
+            for (d = 0; d < n; ++d) {
+                offset[d] += blockSize[d];
+                if (offset[d] <= max[d])
+                    break;
+                else
+                    offset[d] = 0;
+            }
+        }
+
+        return true;
+    }
+
     public static <T extends net.imglib2.type.NativeType<T>> void validateAndRetry(
             net.imglib2.RandomAccessibleInterval<T> source,
             org.janelia.saalfeldlab.n5.N5Writer n5,
@@ -46,10 +86,8 @@ public class N5BlockValidateAndRetry {
         int retry = RETRY_NUM;
         boolean valid = false;
         while(!valid && retry > 0) {
-            System.out.println( "validating block "+ Arrays.toString(gridOffset));
-            valid = ValidateN5Block(n5, dataset, gridOffset);
+            valid = ValidateN5Blocks(source, n5, dataset, gridOffset, retry);
             if (!valid) {
-                System.err.println( "The n5 block "+ Arrays.toString(gridOffset) +" is corrupted. retrying... " + retry);
                 try {
                     Thread.sleep(WAIT_TIME * 1000);
                 } catch (InterruptedException e) {
